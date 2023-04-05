02228ce7604cdb63f547c21be3ff6b85fb3e74f1
slivcaigor <slivcaigor@gmail.com>
2023-04-05 17:41:14 +0200

    Snack 12

---
+++ Snack12/Program.cs
@@ -0,0 +1,27 @@
+﻿//  12. Scrivere una funzione per verificare se un numero è pari o dispari. Quindi chiedere un numero all'utente e comunicargli se è pari o dispari.
+
+Console.Write("Inserisci un numero: ");
+
+int numero;
+
+while (!int.TryParse(Console.ReadLine(), out numero))
+{
+    Console.WriteLine("Numero non valido. Inserisci solo numeri.");
+    Console.Write("Inserisci un numero: ");
+}
+
+if (VerificaPariDispari(numero))
+{
+    Console.WriteLine("Il numero è pari.");
+}
+else
+{
+    Console.WriteLine("Il numero è dispari.");
+}
+
+Console.ReadLine();
+
+ static bool VerificaPariDispari(int numero)
+{
+    return numero % 2 == 0;
+}
