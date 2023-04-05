a80de531f4d25c95e279158f2ea990c91923241f
slivcaigor <slivcaigor@gmail.com>
2023-04-05 16:10:38 +0200

    Snack 7

---
+++ Snack7/Program.cs
@@ -0,0 +1,26 @@
+﻿// 7. Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
+
+int[] numeri = new int[4];
+int count = 0;
+while (count < 4)
+{
+    Console.Write("Inserisci un numero: ");
+    if (int.TryParse(Console.ReadLine(), out int numero))
+    {
+        if (numero % 2 != 0)
+        {
+            numeri[count] = numero;
+            count++;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Numero non valido. Riprova.");
+    }
+}
+Console.WriteLine("I numeri dispari inseriti sono:");
+for (int i = 0; i < numeri.Length; i++)
+{
+    Console.WriteLine(numeri[i]);
+}
+Console.ReadLine();
