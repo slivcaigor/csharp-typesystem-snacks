c8c0c1e85e66078a1b972e0174f91f260de31ea8
slivcaigor <slivcaigor@gmail.com>
2023-04-05 15:48:56 +0200

    Snack 5

---
+++ Snack5/Program.cs
@@ -0,0 +1,20 @@
+﻿// 5. Il software chiede all’utente di inserire un numero. Se il numero inserito è pari, stampa il numero, se è dispari, stampa il numero successivo.
+
+
+Console.Write("Inserisci un numero: ");
+int numero;
+while (!int.TryParse(Console.ReadLine(), out numero))
+{
+    Console.Write("Inserisci un numero valido: ");
+}
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine($"Il numero {numero} è pari!");
+}
+else
+{
+    Console.WriteLine($"Il numero {numero} è dispari, il successivo è: {numero + 1}");
+}
+
+Console.ReadLine();
