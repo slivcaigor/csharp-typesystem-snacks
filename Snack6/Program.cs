9a3fb14e897881353d02d06f5de57b82b7c815bb
slivcaigor <slivcaigor@gmail.com>
2023-04-05 16:03:36 +0200

    Snack 6

---
+++ Snack6/Program.cs
@@ -0,0 +1,27 @@
+﻿//  6. In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby. Chiedi all’utente il suo nome e comunicagli se può partecipare o meno alla festa.
+
+string[] invitati = { "Ibrahim", "Luigi", "Jessica", "Tommaso", "Romina" };
+
+while (true)
+{
+    Console.Write("Inserisci il tuo nome: ");
+    string nome = Console.ReadLine() ?? string.Empty;
+    if (string.IsNullOrWhiteSpace(nome) || nome.Any(char.IsDigit))
+    {
+        Console.WriteLine("Inserisci un nome valido.");
+    }
+    else if (Array.IndexOf(invitati, nome) >= 0)
+    {
+        Console.WriteLine("Sei invitato alla festa del grande Gatsby!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Purtroppo non sei invitato alla festa del grande Gatsby.");
+        break;
+    }
+}
+Console.ReadLine();
+
+
+
